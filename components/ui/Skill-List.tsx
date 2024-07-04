d62c0a8600c221ddd086bf3f09964ae9ebcfd7b6
Belsetbut <146377681+Belsetbut@users.noreply.github.com>
2024-07-04 21:53:13 +0200

    Title and skills are working now

---
+++ components/ui/Skill-List.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+export const skillItems = [
+  {
+    title: "Coding",
+    description: "I love Coding ... jahhhhhh es gehthththuth", //hat 30 min gedauert. ist 22 uhr krieg gleich richtig Ärger
+    link: "/link1",
+  },
+  {
+    title: "Skill 2",
+    description: "Description for skill 2",
+    link: "/link2",
+  },
+  {
+    title: "Skill 3",
+    description: "Description for skill 3",
+    link: "/link3",
+  },
+];
