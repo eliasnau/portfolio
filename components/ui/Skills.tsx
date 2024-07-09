ff1a44aa78e0ca4ac802b5138277e1aa7d8e02c3
Belsetbut <146377681+Belsetbut@users.noreply.github.com>
2024-07-09 19:26:16 +0200

    FIX now the border under Skills will show up wile in white mode in safari

--- components/ui/Skills.tsx
+++ components/ui/Skills.tsx
@@ -3,9 +3,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
 
-
 export const Skills = ({
-
   items = [],
   className,
 }: {
@@ -23,20 +21,18 @@ export const Skills = ({
       className={cn(
         "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10",
         className
-      )}
-    >
+      )}>
       {items.map((item, idx) => (
         <Link
           href={item?.link}
           key={item?.link}
           className="relative group block p-2 h-full w-full"
           onMouseEnter={() => setHoveredIndex(idx)}
-          onMouseLeave={() => setHoveredIndex(null)}
-        >
+          onMouseLeave={() => setHoveredIndex(null)}>
           <AnimatePresence>
             {hoveredIndex === idx && (
               <motion.span
-                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block rounded-3xl"
+                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block rounded-3xl "
                 layoutId="hoverBackground"
                 initial={{ opacity: 0 }}
                 animate={{
@@ -70,10 +66,9 @@ export const Card = ({
   return (
     <div
       className={cn(
-        "rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+        "rounded-2xl h-full w-full p-4 overflow-hidden bg-black border dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
         className
-      )}
-    >
+      )}>
       <div className="relative z-50">
         <div className="p-4">{children}</div>
       </div>
@@ -107,8 +102,7 @@ export const CardDescription = ({
       className={cn(
         "mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm",
         className
-      )}
-    >
+      )}>
       {children}
     </p>
   );
