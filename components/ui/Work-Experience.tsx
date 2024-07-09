a4aae4533409dd9f1f52a6a38dffdd38c25dc51c
Belsetbut <146377681+Belsetbut@users.noreply.github.com>
2024-07-09 19:02:40 +0200

    made it better for phone users by having one card per row

--- components/ui/Work-Experience.tsx
+++ components/ui/Work-Experience.tsx
@@ -20,7 +20,7 @@ export const Work = ({
   return (
     <div
       className={cn(
-        "grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4  xl:grid-cols-5 py-10",
+        "grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4  xl:grid-cols-5 py-10",
         className
       )}>
       {items.map((item, idx) => (
