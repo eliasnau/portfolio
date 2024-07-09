42ca24508ba648d762378c6da02d72b4b8333157
Belsetbut <146377681+Belsetbut@users.noreply.github.com>
2024-07-09 12:35:37 +0200

    did some stuff

--- components/ui/Skill-List.tsx
+++ components/ui/Skill-List.tsx
@@ -3,18 +3,27 @@
 export const skillItems = [
   {
     title: "Coding",
-    description: "I love Coding ... jahhhhhh es gehthththuth", //hat 30 min gedauert. ist 22 uhr krieg gleich richtig Ärger
-    link: "/link1",
+    description:
+      "I love Coding, alone or with a friend. Elias (You can look at his site by clicking this Card)", //hat 30 min gedauert. ist 22 uhr krieg gleich richtig Ärger
+    link: "https://elias.codity.net",
   },
   {
-    title: "Skill 2",
-    description: "Description for skill 2",
+    title: "Also coding",
+    description:
+      "I already coded with react, Unreal Engine 5, Unity, a bit of python and also a bit of java because I tried to make a Minecraft mod.",
     link: "/",
   },
   {
-    title: "Skill 3",
-    description: "Description for skill 3",
-    link: "/",
+    title: "Sports",
+    description:
+      "I love sport and especially athletics. I'am in a sports club named Sportverein Schloßberg or short SVS. ",
+    link: "https://www.sv-schlossberg.de/index.php/sportangebot/leichtathletik/aktuelles?layout=*",
+  },
+  {
+    title: "Still sport",
+    description:
+      "I love pole vault and ok good at it. For example i'm 4 in Bayern. But I also do all the other things you can do like long jump etc.  .",
+    link: "https://dlvbl.laportal.net/Performances?performanceList=d055b92c-6b54-43b3-b7bd-4a1f12a2dcf4&eventcode=PV&classcode=M15&environment=1&year=2024&showForeigners=1",
   },
   {
     title: "Skill 4",
@@ -22,13 +31,8 @@ export const skillItems = [
     link: "/",
   },
   {
-    title: "Skill 4",
-    description: "Desc for skill 4",
-    link: "/",
-  },
-  {
-    title: "Skill 4",
-    description: "Desc for skill 4",
+    title: "Skill 5",
+    description: "Desc for skill 5",
     link: "/",
   },
 ];
